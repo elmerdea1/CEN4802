bdb4bb5b6b87e3819e4e80f3d20c3a1e81d07ca8
Elmer Dea <srd010@gmail.com>
2018-10-14 11:41:51 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package week07;
+
+/**
+ * @author Elmer Dea
+ *
+ */
+public class HelloWorld 
+{
+	/**
+	 * Returns a preset message
+	 * @return an important message
+	 */
+	public String display()
+	{
+		return "Hello World from Elmer Dea";
+	}
+}
+
+
